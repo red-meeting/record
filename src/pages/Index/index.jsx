e6df51aeb4fa182437cd3dc43e8327f2f282c4fb
jjlstruggle <2020213994@stu.cqupt.edu.cn>
2021-11-21 14:45:17 +0800

    update

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -3,36 +3,31 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router";
 export default function Index({ audio, setIsHide }) {
   const navigate = useNavigate();
+  let timer;
   useEffect(() => {
     setIsHide(false);
-    audio.play();
+    audio.play().catch((e) => {
+      audio.currentTime = 0;
+      audio.pause();
+      timer = setInterval(() => {
+        audio
+          .play()
+          .then((e) => {
+            clearInterval(timer);
+          })
+          .catch((e) => {});
+      });
+    });
   }, []);
   return (
     <div className="index">
-      <h1>时代唱片机</h1>
-      <div className="title">带你聆听建党百年的歌声嘹亮</div>
-      <div className="tip">-十九届六中全会专题-</div>
-      <img
-        src={chanpian}
-        alt="图片加载失败,请尝试刷新"
-        style={{
-          width: "100%",
-          height: "36vh",
-          marginTop: "3vh",
-          marginBottom: "6vh",
-        }}
-      />
-      <div className="title">建议配搭耳机体验，效果更佳哟~</div>
       <button
         className="btn"
         onClick={() => {
-          navigate("/record");
+          navigate("/record", { replace: true });
           setIsHide(true);
         }}
-      >
-        一起聆听百年歌声
-      </button>
-      <div className="tag">©红岩网校工作站</div>
+      />
     </div>
   );
 }
